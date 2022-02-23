b4c2279e91e621f4a89841820d633ee70e09297a
lonelymoon <lonelymoon@aliyun.com>
2022-02-23 17:10:28 +0800

    Initial commit

---
+++ src/test/java/wang/lonelymoon/javastudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.lonelymoon.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
