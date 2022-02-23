0a43c761c5bf8d595ffa39f714c16586f9df25ee
lonelymoon <lonelymoon@aliyun.com>
2022-02-23 17:30:51 +0800

    .

---
+++ src/main/java/wang/lonelymoon/javastudy/core/util/Func.java
@@ -0,0 +1,15 @@
+package wang.lonelymoon.javastudy.core.util;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * 常用工具类
+ * @author lonelymoon
+ */
+public class Func {
+
+    public static void main(String[] args) {
+        boolean blank = StrUtil.isBlank("");
+        System.out.println(blank);
+    }
+}
