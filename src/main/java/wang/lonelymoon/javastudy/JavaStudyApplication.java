ae4e212c6712ac287efc163ea3f4e4addbd924cc
lonelymoon <lonelymoon@aliyun.com>
2022-02-24 10:34:53 +0800

    .

--- src/main/java/wang/lonelymoon/javastudy/JavaStudyApplication.java
+++ src/main/java/wang/lonelymoon/javastudy/JavaStudyApplication.java
@@ -3,6 +3,9 @@ package wang.lonelymoon.javastudy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lonelymoon
+ */
 @SpringBootApplication
 public class JavaStudyApplication {
 
